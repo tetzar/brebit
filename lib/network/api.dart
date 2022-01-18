7346ac0000040c56941401be5c6b20395e00ddb9
hwavy <haruki4dev@gmail.com>
2022-01-18 17:30:29 +0900

    changes network.

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -10,9 +10,9 @@ import 'auth.dart';
 
 class   Network {
   // for emulator
-  static final String _url = 'http://127.0.0.1:80';
+  // static final String _url = 'http://127.0.0.1:80';
   // マイハウス
-  // static final String _url = 'http://192.168.3.40:80';
+  static final String _url = 'http://192.168.3.40:80';
   // イマジナリーハウス
   // static final String _url = 'http://192.168.248.224:80';
 
